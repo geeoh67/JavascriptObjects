14cccc49b7cc77e73fab33134079642f065053fc
Greg Oliphant <goliphant@esi-estech.com>
2016-01-27 09:47:22 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// you need to build 4 objects here.
+// person, skills, education, onlineClasses
+
+// person includes name, email, picture, github
+// skills is an array of skills
+// education includes school, degree, major, location, date
+// online classes will include provider, and an array of courses with title and
+// date
+
+// all content must use the jquery append feature.
+
+// html page should be styled with css.
+
+// each object will have it's own display function as the last property
+
+for(var i = 0; i < yourobject.length; i++) { 
+    
+}
+
+
